654cfdfb5e11c99e91028097d2099363b6ceb3f6
markovamaria <maria.markova@intel.com>
2023-08-17 18:32:21 +0200

    Updated python 3.10 to 3.11 fir Ubuntu 23.04

---
+++ tools/env_settings_ubuntu23_04.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -x
+if [ "$1" = "" ]
+then 
+    echo "1st param - server or client"
+    exit
+fi
+
+if [ $1 = 'client' ] || [ $1 = 'all' ] && [ "$2" = "" ]
+then 
+    echo "2st param - redis-benchmarks version, e.g. 68"
+    exit
+fi
+
+
+sudo apt-get update
+sudo apt update
+sudo apt-get install iperf
+#supervisorctl stop 
+
+
+if [ $1 = 'client' ] || [ $1 = 'all' ]
+then 
+    export BENCH_VERS=$2
+    sudo DEBIAN_FRONTEND=noninteractive apt install python3.11-full
+    sudo apt install python3.11-venv
+    python3.11 -m venv env$BENCH_VERS
+    source env$BENCH_VERS/bin/activate #- working in this environment.
+
+    sudo DEBIAN_FRONTEND=noninteractive apt install docker.io -y
+    #sudo apt install supervisor -y
+    pip3 install -r req.txt
+    # get latest or from master ( pip install git+https://github.com/redis/redis-benchmarks-specification.git)
+    pip3 install redis-benchmarks-specification==0.1.$BENCH_VERS # on fixed on July 6
+    pip install pandas
+    
+    sudo groupadd docker # required on NEW host
+    sudo usermod -aG docker $USER
+    
+    ls env$BENCH_VERS/bin/activate
+fi
+
+if [ $1 = 'server' ] || [ $1 = 'all' ]
+then 
+    sudo DEBIAN_FRONTEND=noninteractive apt install pkg-config
+    sudo apt install make
+    sudo apt install numactl
+    sudo apt install net-tools
+
+    # install compilers:
+    echo --- Instal GCC compiler ---
+    sudo DEBIAN_FRONTEND=noninteractive apt install gcc
+    gcc --version
+fi
+
+
+echo --- Installation is finished, rebooting ... --
+sudo reboot
+
